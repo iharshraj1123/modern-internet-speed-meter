d8403511f896ea46ceffd096eecd39a14ee7d4ae
Harsh Raj <iharshraj1123@gmail.com>
2026-07-20 17:39:18 +0530

    Fix windres target architecture error in build.rs for GNU MinGW toolchain

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-    tauri_build::build()
+    if std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "gnu" {
+        if std::env::var("WINDRES_FLAGS").is_err() {
+            std::env::set_var("WINDRES_FLAGS", "-F pe-x86-64");
+        }
+    }
+    tauri_build::build();
 }
