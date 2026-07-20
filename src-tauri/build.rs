2bdf18e6b731f5da98635127e2d1ed3f8dd0fdc0
Harsh Raj <iharshraj1123@gmail.com>
2026-07-20 17:46:46 +0530

    Configure MSVC target to avoid legacy MinGW windres errors

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    let mut attributes = tauri_build::Attributes::new();
-    if std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "gnu" {
-        let windows = tauri_build::WindowsAttributes::new().window_icon_path("");
-        attributes = attributes.windows_attributes(windows);
-    }
-    tauri_build::try_build(attributes).expect("failed to run tauri-build script");
+    tauri_build::build();
 }
