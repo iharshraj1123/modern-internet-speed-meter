ee8968e1b4e8e96d3d3da937096f07fb980acecd
Harsh Raj <iharshraj1123@gmail.com>
2026-07-20 17:42:05 +0530

    Fix AsRef<Path> trait bound in build.rs window_icon_path

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut attributes = tauri_build::Attributes::new();
     if std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "gnu" {
-        let windows = tauri_build::WindowsAttributes::new().window_icon_path(None as Option<&str>);
+        let windows = tauri_build::WindowsAttributes::new().window_icon_path("");
         attributes = attributes.windows_attributes(windows);
     }
     tauri_build::try_build(attributes).expect("failed to run tauri-build script");
